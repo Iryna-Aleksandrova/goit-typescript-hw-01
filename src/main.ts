471ff4f63ef060a8069906e37bb6ba19cc14e86b
Iryna-Aleksandrova <ira_aleksandrova@ukr.net>
2025-03-10 20:48:01 +0000

    sem 4ts

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import "./style.css";
 import "./basic/1.ts";
 import "./basic/2.ts";
 import "./basic/3.ts";
-import "./basic/4.ts";
+// import "./basic/4.ts";
 import "./basic/5.ts";
 import "./basic/6.ts";
 import "./basic/7.ts";
